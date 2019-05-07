958cd6c44c0a8df6b66de44bae7b9f86ade7026e
Jean-Michel Carter <jeanmichel.carter@gmail.com>
2019-05-06 23:43:21 -0400

    Add build folder to gitignore ... refactor main

--- main.cpp
+++ main.cpp
@@ -9,13 +9,12 @@ int main() {
   std::ifstream file(filename, std::ios::in);
 //  std::ifstream file("../files/smallPoem.txt", std::ios::in);
   if(file.is_open()) {
-
     auto parser = ConcordanceParser{std::move(file)};
-
     std::cout << parser.getConcordance();
   }
   else {
     std::cout << "Sorry, no files with the given filename was found.\n";
   }
+
   return 0;
 }
